5504bb2946c3832cd7218f2d9076a4b6d67dbe76
Alex Izvorski <aizvorski@gmail.com>
2017-03-26 12:43:42 -0700

    net run: modify to use datagen.py and net.py

--- net_run.py
+++ net_run.py
@@ -1,14 +1,14 @@
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, merge, Convolution3D, MaxPooling3D, UpSampling3D, GlobalAveragePooling3D, Dense, Flatten, Dropout, Activation
-from keras.layers.normalization import BatchNormalization
-from keras.layers.advanced_activations import ELU
 from keras.optimizers import SGD, Adam, Nadam
 
 from keras_tqdm import TQDMNotebookCallback
 
 import data
-import importlib; importlib.reload(data)
+import datagen
+import net
+
+#import importlib; importlib.reload(data)
 import random
 import scipy.ndimage.interpolation
 
@@ -21,144 +21,10 @@ patient_ids = data.ndsb17_get_patient_ids()
 
 X_nodules, diams = data.ndsb17_get_all_nodules(vsize, df_nodes)
 
-# X_mean, X_std = -378.9, 475.913 # for LUNA16
-X_mean, X_std = -298.099, 436.168
+gen = datagen.batch_generator(vsize, patient_ids, X_nodules[:-50], diams[:-50])
 
 
-def make_augmented(vsize, volume, X_nodules):
-    idx = random.choice(range(len(X_nodules)))
-    nodule = X_nodules[idx]
-    # randomly flip or not flip each axis
-    if random.choice([True, False]):
-        nodule = nodule[::-1,:,:]
-    if random.choice([True, False]):
-        nodule = nodule[:,::-1,:]
-    if random.choice([True, False]):
-        nodule = nodule[:,:,::-1]
-    mask = data.compose_make_mask(vsize, diam=diams[idx]+6, sigma=(diams[idx]+6)/8)
-    volume_aug = data.compose_max2(volume, nodule, mask)
-    return volume_aug
-
-def sample_generator(vsize, patient_ids, X_nodules, diams):
-    n = 0
-    n_aug = 0
-
-    central_mask = data.compose_make_mask(vsize, diam=6+6, sigma=(6+6)/8)
-    
-    while True:
-        if n % 1000 == 0:
-            try:
-                pid = random.choice(patient_ids)
-                image_ = data.ndsb17_get_image(pid)
-                segmented_image_ = data.ndsb17_get_segmented_image(pid)
-
-                image, segmented_image = image_, segmented_image_
-                n+=1
-                # segpack = np.packbits(segmented_image, axis=0)
-                # info = data.luna16_get_info(pid)
-            except Exception as e:
-                #print(pid, repr(e))
-                continue
-            
-        pos = np.asarray([ np.random.randint(k, image.shape[k] - vsize[k]) for k in range(3) ])
-        segmented_volume = segmented_image[pos[0]:pos[0]+vsize[0], pos[1]:pos[1]+vsize[1], pos[2]:pos[2]+vsize[2]]
-        if np.count_nonzero(segmented_volume) == 0:
-            continue
-#         segpack_volume = segpack[pos[0]//8:(pos[0]+vsize[0])//8, pos[1]:pos[1]+vsize[1], pos[2]:pos[2]+vsize[2]]
-#         if np.count_nonzero(segpack_volume) == 0:
-#            continue
-        volume = image[pos[0]:pos[0]+vsize[0], pos[1]:pos[1]+vsize[1], pos[2]:pos[2]+vsize[2]]
-#         overlap = np.mean(segmented_volume)
-#         density = np.mean(volume)
-        central_density = np.mean((volume+1000) * central_mask) / np.mean(central_mask) - 1000
-    
-        is_augmented = False
-        if central_density < -500 and np.random.choice([True, False]):
-            volume = make_augmented(vsize, volume, X_nodules)
-            is_augmented = True
-            n_aug += 1
-            
-        n+=1
-
-        yield volume, is_augmented
-        
-def batch_generator(vsize, patient_ids, X_nodules, diams):
-    gen = sample_generator(vsize, patient_ids, X_nodules, diams)
-    batch_size = 64
-    while True:
-        X = np.zeros((batch_size, 32,32,32,1), dtype=np.float32)
-        y = np.zeros((batch_size, 2), dtype=np.int)
-        for n in range(batch_size):
-            volume, is_augmented = next(gen)
-            X[n,:,:,:,0] = volume
-            if is_augmented:
-                y[n,1] = 1
-            else:
-                y[n,0] = 1
-        X = (X - X_mean)/X_std
-        X = scipy.ndimage.interpolation.zoom(X, (1, 0.5, 0.5, 0.5, 1), order=1)
-        yield X, y
-
-gen = batch_generator(vsize, patient_ids, X_nodules[:-50], diams[:-50])
-
-
-vsize = (16, 16, 16)
-
-
-
-def model3d(vsize, do_features=False):
-    inputs = Input(vsize + (1,))
-    sz = 48
-    alpha = 1.5
-    
-    def conv3dparams(**replace_params):
-        params = { 'activation':ELU(), 'border_mode':'valid', 'init': 'he_normal' }
-        params.update(replace_params)
-        return params
-
-    x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(inputs)
-    x = BatchNormalization()(x)
-
-    sz = int(sz * alpha)
-    x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(x)
-    x = BatchNormalization()(x)
-    x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(x)
-    x = BatchNormalization()(x)
-    # x = MaxPooling3D(pool_size=(2, 2, 2))(x)
-
-    sz = int(sz * alpha)
-    x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(x)
-    x = BatchNormalization()(x)
-    x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(x)
-    x = BatchNormalization()(x)
-    # x = MaxPooling3D(pool_size=(2, 2, 2))(x)
-
-    sz = int(sz * alpha)
-    x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(x)
-    x = BatchNormalization()(x)
-    x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(x)
-    x = BatchNormalization()(x)
-    # x = MaxPooling3D(pool_size=(2, 2, 2))(x)
-
-    sz = int(sz * alpha)
-    # x = Convolution3D(sz, 3, 3, 3, **conv3dparams)(x)
-    # x = Convolution3D(sz, 3, 3, 3, **conv3dparams)(x)
-    # x = BatchNormalization()(x)
-
-    x = Convolution3D(sz, 2, 2, 2, **conv3dparams())(x)
-    x = BatchNormalization()(x)
-    x = Convolution3D(sz, 1, 1, 1, **conv3dparams(border_mode='same'))(x)
-    x = BatchNormalization()(x)
-    x = Convolution3D(2, 1, 1, 1, **conv3dparams(activation='linear', border_mode='same'))(x)
-    if not do_features:
-        x = Flatten()(x)
-        x = Activation('softmax')(x)
-
-    model = Model(input=inputs, output=x)
-
-    return model
-
-model = model3d(vsize)
+model = net.model3d((16, 16, 16))
 print(model.summary())
 
 
