64a532ec5f529d097ea696dee2208d2aa8d4dbda
Alex Izvorski <aizvorski@gmail.com>
2017-03-26 14:56:11 -0700

    Net: process tiled volumes, do point-wise softmax - these may go in utils

--- net.py
+++ net.py
@@ -2,6 +2,7 @@ from keras.models import Model
 from keras.layers import Input, merge, Convolution3D, MaxPooling3D, UpSampling3D, GlobalAveragePooling3D, Dense, Flatten, Dropout, Activation
 from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import ELU
+import numpy as np
 
 
 def model3d(vsize, do_features=False):
@@ -55,3 +56,31 @@ def model3d(vsize, do_features=False):
     model = Model(input=inputs, output=x)
 
     return model
+
+
+def make_full_result(model, image):
+    s = 49
+    d = 64
+    m = 8
+    full_result = np.zeros((image.shape[0]+d, image.shape[1]+d, image.shape[2]+d, 2), dtype=np.float32)
+    full_result[:,:,:,0] = 1e+6
+    for i in range(0, int(np.ceil(image.shape[0]/s))):
+        for j in range(0, int(np.ceil(image.shape[1]/s))):
+            for k in range(0, int(np.ceil(image.shape[2]/s))):
+                input_ = image[i*s:i*s+d,j*s:j*s+d,k*s:k*s+d]
+                if input_.shape != (d,d,d):
+                    input_ = np.pad(input_, ((0, d-input_.shape[0]), (0, d-input_.shape[1]), (0, d-input_.shape[2])), 'constant')
+                result = model.predict(input_.reshape((1,d,d,d,1)), batch_size=1)
+                full_result[i*s+m:(i+1)*s+m, j*s+m:(j+1)*s+m, k*s+m:(k+1)*s+m,:] = result
+    return full_result[0:image.shape[0], 0:image.shape[1], 0:image.shape[2]]
+
+
+def softmax_activations(x):
+    r = x.reshape((-1,2))
+    r_max = np.max(r, axis=-1)
+    r_max = r_max[...,None]
+    r_sum = np.sum(np.exp(r - r_max), axis=-1)
+    r_sum = r_sum[...,None]
+    p = np.exp(r - r_max) / r_sum
+    p = p.reshape(x.shape)
+    return p
