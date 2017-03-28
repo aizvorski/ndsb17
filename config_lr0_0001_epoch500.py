de670294a11934d6bfccc11a1810d43d8ec2f380
Alex Izvorski <aizvorski@gmail.com>
2017-03-28 02:53:58 -0700

    More configs

---
+++ config_lr0_0001_epoch500.py
@@ -0,0 +1,5 @@
+num_epochs = 500
+samples_per_epoch = 10000
+lr  = 0.0001
+feature_sz = 48
+feature_alpha = 1.5
---
+++ config_sz96_alpha1_5.py
@@ -0,0 +1,5 @@
+num_epochs = 500
+samples_per_epoch = 10000
+lr  = 0.001
+feature_sz = 96
+feature_alpha = 1.5
