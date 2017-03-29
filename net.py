a6fa1ff2499045f8065461a7269b6582d84ea9fa
Alex Izvorski <aizvorski@gmail.com>
2017-03-28 18:52:05 -0700

    Dropout

--- net.py
+++ net.py
@@ -1,5 +1,5 @@
 from keras.models import Model
-from keras.layers import Input, merge, Convolution3D, MaxPooling3D, UpSampling3D, GlobalAveragePooling3D, Dense, Flatten, Dropout, Activation
+from keras.layers import Input, merge, Convolution3D, MaxPooling3D, UpSampling3D, GlobalAveragePooling3D, Dense, Flatten, Dropout, Activation, SpatialDropout3D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import ELU
 import numpy as np
@@ -9,36 +9,38 @@ def model3d(vsize, sz=48, alpha=1.5, do_features=False):
     inputs = Input(vsize + (1,))
     
     def conv3dparams(**replace_params):
-        params = { 'activation':'linear', 'border_mode':'valid', 'init': 'he_normal' }
+        params = { 'activation':ELU(), 'border_mode':'valid', 'init': 'he_normal' }
         params.update(replace_params)
         return params
 
     x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(inputs)
-    x = ELU()(BatchNormalization()(x))
+    x = BatchNormalization()(x)
 
     sz = int(sz * alpha)
     x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(x)
-    x = ELU()(BatchNormalization()(x))
+    x = BatchNormalization()(x)
     x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(x)
-    x = ELU()(BatchNormalization()(x))
+    x = BatchNormalization()(x)
+    x = SpatialDropout3D(0.2)(x)
 
     sz = int(sz * alpha)
     x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(x)
-    x = ELU()(BatchNormalization()(x))
+    x = BatchNormalization()(x)
     x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(x)
-    x = ELU()(BatchNormalization()(x))
+    x = BatchNormalization()(x)
 
     sz = int(sz * alpha)
     x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(x)
-    x = ELU()(BatchNormalization()(x))
+    x = BatchNormalization()(x)
     x = Convolution3D(sz, 3, 3, 3, **conv3dparams())(x)
-    x = ELU()(BatchNormalization()(x))
+    x = BatchNormalization()(x)
+    x = SpatialDropout3D(0.5)(x)
 
     sz = int(sz * alpha)
     x = Convolution3D(sz, 2, 2, 2, **conv3dparams())(x)
-    x = ELU()(BatchNormalization()(x))
+    x = BatchNormalization()(x)
     x = Convolution3D(sz, 1, 1, 1, **conv3dparams())(x)
-    x = ELU()(BatchNormalization()(x))
+    x = BatchNormalization()(x)
     x = Convolution3D(2, 1, 1, 1, **conv3dparams(activation='linear', border_mode='same'))(x)
     if not do_features:
         x = Flatten()(x)
