Fix scaling border bug

datagen.py
@@ -107,7 +107,7 @@ def batch_generator(vsize, patient_ids, X_nodules, diams):
             else:
                 y[n,0] = 1
         X = (X - X_mean)/X_std
-        X = scipy.ndimage.interpolation.zoom(X, (1, 0.5, 0.5, 0.5, 1), order=1)
+        X = skimage.transform.downscale_local_mean(X, (1,2,2,2,1), clip=False)
         yield X, y
 
 
@@ -133,5 +133,5 @@ def batch_generator_ab(vsize, patient_ids, X_nodules_a, diams_a, X_nodules_b, di
             X[n,:,:,:,0] = volume
             n += 1
         X = (X - X_mean)/X_std
-        X = scipy.ndimage.interpolation.zoom(X, (1, 0.5, 0.5, 0.5, 1), order=1)
+        X = skimage.transform.downscale_local_mean(X, (1,2,2,2,1), clip=False)
         yield X, y