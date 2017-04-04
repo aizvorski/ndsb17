8b56c76fdc1352ff8c16792f49aff6d3104e825a
Alex Izvorski <aizvorski@gmail.com>
2017-04-03 19:28:36 -0700

    Datagen: add downscale option

--- datagen.py
+++ datagen.py
@@ -116,7 +116,7 @@ def batch_generator(vsize, patient_ids, X_nodules, diams):
         yield X, y
 
 
-def batch_generator_ab(vsize, patient_ids, X_nodules_a, diams_a, X_nodules_b, diams_b):
+def batch_generator_ab(vsize, patient_ids, X_nodules_a, diams_a, X_nodules_b, diams_b, do_downscale=True):
     gen_a = sample_generator(vsize, patient_ids, X_nodules_a, diams_a)
     gen_b = sample_generator(vsize, patient_ids, X_nodules_b, diams_b)
     batch_size = 64
@@ -138,5 +138,6 @@ def batch_generator_ab(vsize, patient_ids, X_nodules_a, diams_a, X_nodules_b, di
             X[n,:,:,:,0] = volume
             n += 1
         X = (X - X_mean)/X_std
-        X = skimage.transform.downscale_local_mean(X, (1,2,2,2,1), clip=False)
+        if do_downscale:
+            X = skimage.transform.downscale_local_mean(X, (1,2,2,2,1), clip=False)
         yield X, y
