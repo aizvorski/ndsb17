74226d3c7bc11bd94118f15935177016f7a80dfb
Alex Izvorski <aizvorski@gmail.com>
2017-04-07 14:43:13 -0700

    Baseline4

---
+++ config_baseline4.py
@@ -0,0 +1,8 @@
+feature_sz = 96
+feature_alpha = 1.5
+optimizer='adam'
+num_epochs = 1300
+samples_per_epoch = 1000
+lr  = 0.001
+lr_step_num_epochs = 1000
+lr_step_multiplier = 0.1
---
+++ config_baseline4_classifier.py
@@ -0,0 +1,10 @@
+feature_sz = 96
+feature_alpha = 1.5
+optimizer='adam'
+num_epochs = 400
+samples_per_epoch = 1000
+lr  = 0.001
+lr_step_num_epochs = 200
+lr_step_multiplier = 0.1
+min_activity_train = 30
+min_activity_predict = 30
